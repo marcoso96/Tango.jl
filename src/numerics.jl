a7716e86c2381c071ccba6a5e2ec0ed49e6a88b2
Obanmarcos <obandomarcos96@gmail.com>
2024-04-16 14:39:48 +0200

    added tests and docs and stuff

--- src/numerics.jl
+++ src/numerics.jl
@@ -1,4 +1,5 @@
 export numderiv
+export tangent
 
 """
     numderiv(f, x_0, epsilon = 1e-7)
@@ -6,7 +7,18 @@ export numderiv
 Return the approximate derivative of f(x) at x_0
 """
 function numderiv(f, x_0, epsilon = 1e-7)
+    df = f(x_0+epsilon)-f(x_0+epsilon)
+    return df/(2epsilon)
+end
 
-    df = f(x_0+epsilon)-f(x_0)
-    return df/epsilon
+"""
+    tangent(f, x_0, epsilon = 1e-7)
+
+Return the approximate tangent of f(x) at x_0
+"""
+
+function tangent(f, x_0, epsilon)
+    slope = numderiv(f, x_0, epsilon)
+    r(x) = slope * (x-x_0) + f(x_0)
+    return r
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,16 @@ using Test
 
 @testset "Numerical derivative" begin
     
-    f(x) = 2x + 1
-    g(x) = x^3 - x
+    f(x) = 2x^2 + 1
 
-    @test numderiv(f, 3, 1) == 2
+    @test numderiv(f, 0) == 0
+
+end
+
+@testset "Tangents" begin
+    
+    f(x) = 2x+1
+    
+    @test tangent(f, 1, 1e-5)(2) == 3
 
 end
