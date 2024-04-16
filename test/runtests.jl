7381b2c985dfae0855758a0565ceabcdf543e2c8
Obanmarcos <obandomarcos96@gmail.com>
2024-04-16 13:43:55 +0200

    Tests numerics

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Tango
 using Test
 
-@testset "Tango.jl" begin
-    # Write your tests here.
+@testset "Numerical derivative" begin
+    
+    f(x) = 2x + 1
+    g(x) = x^3 - x
+
+    @test numderiv(f, 3, 1) == 2
+    @test numderiv(g, 0) = -1 atol=0.01
+
 end
